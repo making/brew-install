3ea7f9615955fa33b2b928654e5c6dcd4218b1ab
Toshiaki Maki <tmaki@pivotal.io>
2020-02-20 01:08:14 +0900

    first commit

---
+++ brew-install.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+$(which jq > /dev/null) || {
+  brew install jq
+}
+
+$(which wget > /dev/null) || {
+  brew install wget
+}
+
+$(which watch > /dev/null) || {
+  brew install watch
+}
+
+$(which kubectl > /dev/null) || {
+  brew install kubectl
+}
+
+$(which stern > /dev/null) || {
+  brew install stern
+}
+
+$(which terraform > /dev/null) || {
+  brew install terraform
+}
+
+$(which az > /dev/null) || {
+  brew install azure-cli
+}
+
+$(which aws > /dev/null) || {
+  brew install awscli
+}
+
+$(which node > /dev/null) || {
+  brew install nodejs
+}
+
+$(which docker > /dev/null) || {
+  brew install docker
+  brew cask install docker
+}
+
+$(which cf > /dev/null) || {
+  brew install cloudfoundry/tap/cf-cli
+}
+
+$(which bosh > /dev/null) || {
+  brew install cloudfoundry/tap/bosh-cli
+}
+
+$(which credhub > /dev/null) || {
+  brew install cloudfoundry/tap/credhub-cli
+}
+
+$(which subl > /dev/null) || {
+  brew cask install sublime-text
+}
+
